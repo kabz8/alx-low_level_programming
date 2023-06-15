1293dd046c3cb33f6b78ac757e18ce7540d47adc
kabz8 <kabaikunjane@gmail.com>
2023-06-15 12:29:19 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,14 @@
 #include<stdio.h>
 /**
- * main - Entry point
+ * main -Entry point
  *
- * Return: Always 0 (success)
- * /
- int main(void)
- {
- 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
-	}
+ * Return:Always 0(Success)
+ */
+int main (void)
+{
+	printf ("size of a char: %d bytes(s)\n",sizeof(char));
+	printf ("size of an int: %d byte(s)\n",sizeof(int));
+	printf ("size of a long int: %d byte(s)\n",sizeof(long int));
+	printf ("size of a float: %d byte(s)\n",sizeof(float));
+	return (0);
+}
