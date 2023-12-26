04ffbb56e0489d31f24ba4ee5046482773ec76fc
root <kabaikunjane@gmail.com>
2023-12-26 10:14:26 -0800

    C - Hash tables

---
+++ 0x1A-hash_tables/3-main.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - get index for key given
+ * @key: key (string) to hash
+ * @size: size of hash table
+ *
+ * Return: index of 'key' using djb2
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+    hash_table_t *ht;
+
+    ht = hash_table_create(1024);
+    hash_table_set(ht, "betty", "cool");
+    return (EXIT_SUCCESS);
+}
